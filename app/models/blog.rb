688de23a1a8f245c900ad01a85653460edb82c74
yuichi oyama <yuichi19991208@gmail.com>
2020-11-01 19:15:10 +0900

    validationを切り出す前

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,4 +10,19 @@ class Blog < ApplicationRecord
   # scope :latest, -> (number = 3){order(created_at: :desc).limit(number)}
   include CommonModule
   test_puts
+
+  validate :title_more_write
+  validate :content_more_write
+  private
+
+  def title_more_write
+    if title.length < 3
+      errors.add(:title, ": 3文字以上書きましょう")
+    end
+  end
+  def content_more_write
+    if content.length < 20
+      errors.add(:content, ": 20文字以上書きましょう")
+    end
+  end
 end
