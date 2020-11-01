0ac32f8ab8a5f97843a353db5508a374f5cc118c
yuichi oyama <yuichi19991208@gmail.com>
2020-11-01 18:07:42 +0900

    topicのcrud機能を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_083703) do
+ActiveRecord::Schema.define(version: 2020_11_01_090328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
