7aa519515180e567b86ac6c8d1b9d6900053e4ea
yuichi oyama <yuichi19991208@gmail.com>
2020-11-01 19:40:43 +0900

    callback切り出し前

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -11,5 +11,15 @@ class Blog < ApplicationRecord
   include CommonModule
   test_puts
 
-  validates_with BlogValidator
+  # validates_with BlogValidator
+
+  before_create :default_title
+  before_create :default_content
+  private
+  def default_title
+    self.title = 'タイトルなし' if self.title.blank?
+  end
+  def default_content
+    self.content = '内容なし' if self.content.blank?
+  end
 end
